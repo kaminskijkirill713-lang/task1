3ae792f5662e2dc10fe3012831440fab9e3031f2
kaminskijkirill713-lang <kaminskijkirill713@gmail.com>
2025-10-18 13:19:45 +0600

    Add files via upload

---
+++ bot_he.py
@@ -0,0 +1,51 @@
+import telebot
+    
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("7965022726:AAH8BPcTr77W_2WnUb1pNLDuT-_pqnad2XU")
+
+
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
+
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
+
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+    'help':
+        u'we have a comands start info and help. '
+}
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+
+@bot.message_handler(commands=['info'])
+def on_info(message):
+    bot.reply_to(message, text_messages['info'])
+
+@bot.message_handler(commands=['help'])
+def on_info(message):
+    bot.reply_to(message, text_messages['help'])
+
+# Запуск бота
+bot.polling()
